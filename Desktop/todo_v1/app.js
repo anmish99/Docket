557b04e7a25aea4470cb85264a92a233af952f73
anurag <amishra1_be18@gmail.com>
2021-07-21 12:20:48 +0530

    v1

---
+++ Desktop/todo_v1/app.js
@@ -0,0 +1,72 @@
+
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+
+var items = [];
+var workItems = [];
+
+app.set('view engine','ejs');
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+app.get("/",function(req,res)
+{
+  var today = new Date();
+
+var options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+  var day = today.toLocaleDateString("en-US",options);
+  res.render("list",{listTitle: day, news: items});
+});
+
+
+
+
+
+app.post("/",function(req,res){
+
+   console.log(req.body);
+
+  var item = req.body.newItem;
+  if(req.body.list === "Work")
+  {
+      workItems.push(item);
+      res.redirect("/work");
+  }
+else{
+     items.push(item);
+     res.redirect("/");
+ }
+});
+
+
+
+
+
+
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle: "Work List",news: workItems});
+});
+
+
+app.get("/about",function(req,res){
+  res.render("about");
+});
+
+
+
+
+
+
+app.listen(3000,function()
+{
+  console.log("server up");
+});
