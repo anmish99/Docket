ecf26813094d72f5a415dd4fc6687b517062fa40
anurag <amishra1_be18@gmail.com>
2021-07-21 19:38:04 +0530

    version 2.0

--- Desktop/todo_v1/app.js
+++ Desktop/todo_v1/app.js
@@ -1,20 +1,39 @@
-
+// jshint esversion:6
 const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
 
-var items = [];
-var workItems = [];
 
 app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item",itemsSchema);
+const item1 = new Item({
+  name: "Welcome to Docket!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new Item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an Item."
+});
 
 
+const defaultItems= [item1, item2, item3];
 
 
 app.get("/",function(req,res)
 {
+
   var today = new Date();
 
 var options = {
@@ -23,7 +42,27 @@ var options = {
   month: "long"
 };
   var day = today.toLocaleDateString("en-US",options);
-  res.render("list",{listTitle: day, news: items});
+
+
+Item.find({},function(err,foundItems)
+{
+  if(foundItems.length === 0)
+  {
+    Item.insertMany(defaultItems,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Successfully saved default items");
+    }
+    });
+  res.redirect("/");
+}
+  else{
+    res.render("list",{listTitle: day, news: foundItems});
+  }
+});
+
+
 });
 
 
@@ -32,23 +71,28 @@ var options = {
 
 app.post("/",function(req,res){
 
-   console.log(req.body);
+  const itemName = req.body.newItem;
 
-  var item = req.body.newItem;
-  if(req.body.list === "Work")
-  {
-      workItems.push(item);
-      res.redirect("/work");
-  }
-else{
-     items.push(item);
-     res.redirect("/");
- }
+const item = new Item({
+  name: itemName
+});
+
+item.save();
+res.redirect("/");
 });
 
 
+app.post("/delete",function(req,res){
+  const checked = (req.body.checkbox);
 
-
+  Item.findByIdAndRemove(checked,function(err){
+  if(!err)
+  {
+    console.log("Successfully deleted");
+    res.redirect("/");
+  }
+})
+});
 
 
 
